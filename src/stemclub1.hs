6423a67ce1549b2196a0f0279afbeccfb3b35666
Galaxy Being <borgauf@gmail.com>
2025-03-03 17:05:44 -0600

    202503031704

--- src/stemclub1.hs
+++ src/stemclub1.hs
@@ -11,3 +11,23 @@ primeFactors n = go [2..] n
       | otherwise = go (tail primes) num
 
 divides d n = rem n d == 0
+
+data Color = Red | Yellow | Blue | Green deriving (Show,Read)
+
+instance Eq Color where
+  Red == Red = True      -- could also be (==) Red Red = True
+  Yellow == Yellow = True
+  Blue == Blue = True
+  Green == Green = True
+  _ == _ = False         -- by now anything else must be false
+
+
+
+ld n = ldf 2 n
+ldf k n | divides k n = k
+        | k^2 > n = n
+        | otherwise = ldf (k+1) n
+
+prime0 n | n < 1 = error "Not a positive integer"
+         | n == 1 = False
+         | otherwise = ld n == n
